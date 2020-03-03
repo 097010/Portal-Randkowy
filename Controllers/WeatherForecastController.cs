ff8be34b6f2a66ff6a2ce3affcb2bf952449f730
Dawid Dominik <dpolanski1999@gmail.com>
2020-03-03 12:31:26 +0100

    Utworzenie Pierwszego modelu Value.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PortalRandkowy.API.Controllers
+{
+    // http://localhost:5000/api/Weather
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"value1","value2","value3","value4"};
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+        [HttpGet]
+        public void Post([FromBody] string value)
+        {
+            
+        }
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
