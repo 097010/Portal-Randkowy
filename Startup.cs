3d310753f3a94ad1bf98b97868a75de6e2fc2052
Dawid Dominik <dpolanski1999@gmail.com>
2020-03-05 18:50:06 +0100

    Łączenie z bazą danych Portal Randkowy (działa)

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace PortalRandkowy.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite("Conection String"));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.AddControllers();
             services.AddMvc(option => option.EnableEndpointRouting = false);
